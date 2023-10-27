49f4c51741bea9072ed0f6da1a984b597033805f
ardenos911 <ardenos@protonmail.com>
2023-10-27 00:02:35 -0400

    models folder and person dart file added

---
+++ lib/models/person.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Person
+{
+   //personal info
+  String? uid;
+  String? imageProfile;
+  String? email;
+  String? password;
+  String? name;
+  int? age;
+  String? gender;
+  String? phoneNo;
+  String? city;
+  String? country;
+  String? profileHeading;
+  String? lookingForInaPartner;
+  int? publishedDateTime;
+
+  //Appearance
+  String? height;
+  String? weight;
+  String? bodyType;
+
+  //Life style
+  String? drink;
+  String? smoke;
+  String? martialStatus;
+  String? haveChildren;
+  String? noOfChildren;
+  String? profession;
+  String? employmentStatus;
+  String? income;
+  String? livingSituation;
+  String? willingToRelocate;
+  String? relationshipYouAreLookingFor;
+
+  //Background - Cultural Values
+  String? nationality;
+  String? education;
+  String? languageSpoken;
+  String? religion;
+  String? ethnicity;
+
+
+
+
+
+}
