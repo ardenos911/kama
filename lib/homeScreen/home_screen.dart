8913367dd9cd0a4cd1c8c9b29881baa9d1e86fc6
ardenos911 <ardenos@protonmail.com>
2023-10-28 03:50:40 -0400

    home_screen file updated 3

--- lib/homeScreen/home_screen.dart
+++ lib/homeScreen/home_screen.dart
@@ -13,6 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black45,
        body: Center(
          child:Text(
            "Welcome to Your Home Page",
