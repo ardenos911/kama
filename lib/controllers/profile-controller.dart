a9ef9434280688a921c83c0178e663f8445e33cf
ardenos911 <ardenos@protonmail.com>
2023-11-02 03:29:08 -0400

    profile-controller completion updates

--- lib/controllers/profile-controller.dart
+++ lib/controllers/profile-controller.dart
@@ -65,4 +65,183 @@ class ProfileController extends GetxController
     }
   }// end of onInit()
 
+  favoriteSentAndFavoriteReceived(String toUserID, String senderName) async
+  {
+    var document = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(toUserID).collection("favoriteReceived").doc(currentUserID)
+        .get();
+
+    //remove the favorite from database
+    if(document.exists)
+    {
+      //remove currentUserID from the favoriteReceived list of that profile person [toUserID]
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(toUserID).collection("favoriteReceived").doc(currentUserID)
+          .delete();
+
+      //remove profile person [toUserID] from the favoriteSent list of the currentUser
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(currentUserID).collection("favoriteSent").doc(toUserID)
+          .delete();
+    }
+    else //mark as favorite //add favorite in database
+        {
+      //add currentUserID to the favoriteReceived list of that profile person [toUserID]
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(toUserID).collection("favoriteReceived").doc(currentUserID)
+          .set({});
+
+      //add profile person [toUserID] to the favoriteSent list of the currentUser
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(currentUserID).collection("favoriteSent").doc(toUserID)
+          .set({});
+
+      //send notification
+      sendNotificationToUser(toUserID, "Favorite", senderName);
+    }
+
+    update();
+  }
+
+  likeSentAndLikeReceived(String toUserID, String senderName) async
+  {
+    var document = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(toUserID).collection("likeReceived").doc(currentUserID)
+        .get();
+
+    //remove the like from database
+    if(document.exists)
+    {
+      //remove currentUserID from the likeReceived list of that profile person [toUserID]
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(toUserID).collection("likeReceived").doc(currentUserID)
+          .delete();
+
+      //remove profile person [toUserID] from the likeSent list of the currentUser
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(currentUserID).collection("likeSent").doc(toUserID)
+          .delete();
+    }
+    else //add-sent like in database
+        {
+      //add currentUserID to the likeReceived list of that profile person [toUserID]
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(toUserID).collection("likeReceived").doc(currentUserID)
+          .set({});
+
+      //add profile person [toUserID] to the likeSent list of the currentUser
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(currentUserID).collection("likeSent").doc(toUserID)
+          .set({});
+
+      //send notification
+      sendNotificationToUser(toUserID, "Like", senderName);
+    }
+
+    update();
+  }
+
+  viewSentAndViewReceived(String toUserID, String senderName) async
+  {
+    var document = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(toUserID).collection("viewReceived").doc(currentUserID)
+        .get();
+
+    if(document.exists)
+    {
+      print("already in view list");
+    }
+    else //add new view in database
+        {
+      //add currentUserID to the viewReceived list of that profile person [toUserID]
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(toUserID).collection("viewReceived").doc(currentUserID)
+          .set({});
+
+      //add profile person [toUserID] to the viewSent list of the currentUser
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(currentUserID).collection("viewSent").doc(toUserID)
+          .set({});
+
+      //send notification
+      sendNotificationToUser(toUserID, "View", senderName);
+    }
+
+    update();
+  }
+
+  sendNotificationToUser(receiverID, featureType, senderName) async
+  {
+    String userDeviceToken = "";
+
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(receiverID)
+        .get()
+        .then((snapshot)
+    {
+      if(snapshot.data()!["userDeviceToken"] != null)
+      {
+        userDeviceToken = snapshot.data()!["userDeviceToken"].toString();
+      }
+    });
+
+    notificationFormat(
+      userDeviceToken,
+      receiverID,
+      featureType,
+      senderName,
+    );
+  }
+
+  notificationFormat(userDeviceToken, receiverID, featureType, senderName,)
+  {
+    Map<String, String> headerNotification =
+    {
+      "Content-Type": "application/json",
+      "Authorization": fcmServerToken,
+    };
+
+    Map bodyNotification =
+    {
+      "body": "you have received a new $featureType from $senderName. Click to see.",
+      "title": "New $featureType",
+    };
+
+    Map dataMap =
+    {
+      "click_action": "FLUTTER_NOTIFICATION_CLICK",
+      "id": "1",
+      "status": "done",
+      "userID": receiverID,
+      "senderID": currentUserID,
+    };
+
+    Map notificationOfficialFormat =
+    {
+      "notification": bodyNotification,
+      "data": dataMap,
+      "priority": "high",
+      "to": userDeviceToken,
+    };
+
+    http.post(
+      Uri.parse("https://fcm.googleapis.com/fcm/send"),
+      headers: headerNotification,
+      body: jsonEncode(notificationOfficialFormat),
+    );
+  }
 }
