6e38e2ac6be825fe1adbbe161502d3b13848736a
ardenos911 <ardenos@protonmail.com>
2023-11-02 02:57:26 -0400

    swipping screen updates

--- lib/tabScreens/swipping_screen.dart
+++ lib/tabScreens/swipping_screen.dart
@@ -4,6 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../controllers/profile-controller.dart';
+
 class SwippingScreen extends StatefulWidget {
   const SwippingScreen({super.key});
 
@@ -11,20 +13,256 @@ class SwippingScreen extends StatefulWidget {
   State<SwippingScreen> createState() => _SwippingScreenState();
 }
 
-class _SwippingScreenState extends State<SwippingScreen> {
+class _SwippingScreenState extends State<SwippingScreen>
+{
+
+  ProfileController profileController = Get.put(ProfileController());
+  String senderName = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.black45,
-        body: Center(
-            child:Text(
-              "Welcome to Your Swipping Screen",
-              style: TextStyle(
-                color: Colors.redAccent,
-                fontSize: 14,
+        body: PageView.builder(
+          itemCount: profileController.allUsersProfileList.length,
+          controller: PageController(initialPage: 0, viewportFraction: 1),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index)
+          {
+            final eachProfileInfo = profileController.allUsersProfileList[index];
+
+            return DecoratedBox(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      eachProfileInfo.imageProfile.toString(),
+                    ),
+                    fit: BoxFit.cover,
+                  )
               ),
-            )
-        )
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+
+                    //filter icon button
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: IconButton(
+                          onPressed: ()
+                          {
+                            applyFilter();
+                          },
+                          icon: const Icon(
+                            Icons.filter_list,
+                            size: 30,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    const Spacer(),
+
+                    //user data
+                    GestureDetector(
+                      onTap: ()
+                      {
+                        profileController.viewSentAndViewReceived(
+                          eachProfileInfo.uid.toString(),
+                          senderName,
+                        );
+
+                        //send user to profile person userDetailScreen
+                        Get.to(UserDetailsScreen(
+                          userID: eachProfileInfo.uid.toString(),
+                        ));
+                      },
+                      child: Column(
+                        children: [
+
+                          //name
+                          Text(
+                            eachProfileInfo.name.toString(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              letterSpacing: 4,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          //age - city
+                          Text(
+                            eachProfileInfo.age.toString() + " ◉ " + eachProfileInfo.city.toString(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              letterSpacing: 4,
+                            ),
+                          ),
+
+                          const SizedBox(
+                            height: 4,
+                          ),
+
+                          //profession and religion
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+
+                              ElevatedButton(
+                                onPressed: (){},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white30,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16)
+                                  ),
+                                ),
+                                child: Text(
+                                  eachProfileInfo.profession.toString(),
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+
+                              const SizedBox(
+                                width: 6,
+                              ),
+
+                              ElevatedButton(
+                                onPressed: (){},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white30,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16)
+                                  ),
+                                ),
+                                child: Text(
+                                  eachProfileInfo.religion.toString(),
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+
+                            ],
+                          ),
+
+                          //country and ethnicity
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+
+                              ElevatedButton(
+                                onPressed: (){},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white30,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16)
+                                  ),
+                                ),
+                                child: Text(
+                                  eachProfileInfo.country.toString(),
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+
+                              const SizedBox(
+                                width: 6,
+                              ),
+
+                              ElevatedButton(
+                                onPressed: (){},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white30,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16)
+                                  ),
+                                ),
+                                child: Text(
+                                  eachProfileInfo.ethnicity.toString(),
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+
+                            ],
+                          ),
+
+                        ],
+                      ),
+                    ),
+
+                    const SizedBox(
+                      height: 14,
+                    ),
+
+                    //image buttons - favorite - chat - like
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+
+                        //favorite button
+                        GestureDetector(
+                          onTap: ()
+                          {
+                            profileController.favoriteSentAndFavoriteReceived(
+                              eachProfileInfo.uid.toString(),
+                              senderName,
+                            );
+                          },
+                          child: Image.asset(
+                            "images/favorite.png",
+                            width: 60,
+                          ),
+                        ),
+
+                        //chat button
+                        GestureDetector(
+                          onTap: ()
+                          {
+                            startChattingInWhatsApp(eachProfileInfo.phoneNo.toString());
+                          },
+                          child: Image.asset(
+                            "images/chat.png",
+                            width: 90,
+                          ),
+                        ),
+
+                        //like button
+                        GestureDetector(
+                          onTap: ()
+                          {
+                            profileController.likeSentAndLikeReceived(
+                              eachProfileInfo.uid.toString(),
+                              senderName,
+                            );
+                          },
+                          child: Image.asset(
+                            "images/like.png",
+                            width: 60,
+                          ),
+                        ),
+
+                      ],
+                    ),
+
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
     );
   }
 }
