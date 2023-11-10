151e65db42a9645f914430edee4d0271d19e313f
ardenos911 <ardenos@protonmail.com>
2023-11-10 04:19:44 -0500

    main.dart permission_notification code added along with dependancy

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:kama_love/authenticationScreen/login_screen.dart';
 import 'package:kama_love/controllers/authentication_controller.dart';
@@ -15,7 +16,14 @@ void main() async
     Get.put(AuthenticationController());
   });
 
-
+ //get permission for receipt of push Notifications
+  await Permission.notification.isDenied.then((value)
+  {
+    if(value)
+    {
+      Permission.notification.request();
+    }
+  });
 
   runApp(const MyApp());
 }
