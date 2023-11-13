4c942b38e1d859465144d451370459d0148b7d52
ardenos911 <ardenos@protonmail.com>
2023-11-13 00:00:21 -0500

    geolocator distance_calculator added vs2

--- lib/tabScreens/distance_calculator.dart
+++ lib/tabScreens/distance_calculator.dart
@@ -6,10 +6,10 @@ Future<double> calculateDistance(String uid1, String uid2) async {
   DocumentSnapshot doc1 = await FirebaseFirestore.instance.collection('users').doc(uid1).get();
   DocumentSnapshot doc2 = await FirebaseFirestore.instance.collection('users').doc(uid2).get();
 
-  double startLatitude = doc1.data()!["lat"];
-  double startLongitude = doc1.data()!['long'];
-  double endLatitude = doc2.data()!['lat'];
-  double endLongitude = doc2.data()!['long'];
+  double startLatitude = doc1.get("lat");
+  double startLongitude = doc1.get('long');
+  double endLatitude = doc2.get('lat');
+  double endLongitude = doc2.get('long');
 
   double distanceInMeters = Geolocator.distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude);
 
