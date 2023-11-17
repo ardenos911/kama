4748956a6b7428bb4eed2a122a35302ffc684580
ardenos911 <ardenos@protonmail.com>
2023-11-17 03:46:09 -0500

    paypal integration started

---
+++ lib/controllers/date_expiration.dart
@@ -0,0 +1,13 @@
+
+
+bool isThirtyDaysPassed(int date1, int date2)
+{
+
+  DateTime dateTime1 = DateTime.fromMillisecondsSinceEpoch(date1);
+
+  DateTime dateTime2 = DateTime.fromMillisecondsSinceEpoch(date2);
+
+  Duration difference = dateTime1.difference(dateTime2);
+
+  return difference.inDays >= 30;
+}
