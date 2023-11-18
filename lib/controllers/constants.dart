40705c6311d39e8dfece3900488bff0380a47a56
ardenos911 <ardenos@protonmail.com>
2023-11-18 01:27:53 -0500

    paypal_controller created vs9

--- lib/controllers/constants.dart
+++ lib/controllers/constants.dart
@@ -3,7 +3,7 @@
 class Constants {
   static final String clientId = '';
   static final String secretKey = '';
-  static final String returnURL = '';
-  static final String cancelURL = '';
+  static final String returnURL = 'https://samplesite.com/return';
+  static final String cancelURL = 'https://samplesite.com/cancel';
 
 }
