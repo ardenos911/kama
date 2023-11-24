5f0e7151158dad11454cbf85196867bda4ae5d0f
ardenos911 <ardenos@protonmail.com>
2023-11-24 02:08:17 -0500

    login screen update forgot password added vs4

--- lib/authenticationScreen/forgot_pw_page.dart
+++ lib/authenticationScreen/forgot_pw_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_text_field_widget.dart';
+
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
 
@@ -8,6 +10,10 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+
+  TextEditingController emailTextEditingController= TextEditingController();
+  TextEditingController passwordTextEditingController= TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +23,38 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
       ),
       body: Column(
           children:[
+            const SizedBox(
+              height:30,
+            ),
             Text('Enter Your Email and we will send you a password reset link'),
+
+            const SizedBox(
+              height:30,
+            ),
+            SizedBox(
+              height:50,
+              width:MediaQuery.of(context).size.width-40,
+              child: CustomTextFieldWidget(
+                editingController: emailTextEditingController,
+                labelText: "Email",
+                iconData: Icons.email_outlined,
+                isObscure: false,
+              ),
+            ),
+            const SizedBox(
+              height:30,
+            ),
+            MaterialButton(
+              onPressed:(){
+
+              },
+              color: Colors.deepPurple[200],
+              child: const Text('Reset Password',
+              style: TextStyle(
+               fontWeight:FontWeight.bold,
+                color:Colors.black),
+                  ),
+            ),
         ]
       )
     );
